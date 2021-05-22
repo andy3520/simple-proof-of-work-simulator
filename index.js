192035c60aa31b9ec5217f3f06a6a9cbe134ef4a
An Nguyen <andy.annguyen3520@gmail.com>
2021-05-22 23:18:39 +0700

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+let startButton = document.getElementById("start");
+startButton.onclick = () => {
+  startButton.disabled = true;
+  startButton.innerText = "Mining...";
+  start();
+  startButton.disabled = false;
+  startButton.innerText = "Start mining";
+};
+
+function start() {
+  let data = getData("data");
+  let complexity = getData("complexity");
+  console.table({
+    complexity,
+    data,
+  });
+  mining(data, +complexity);
+}
+
+function mining(data, complexity) {
+  const startTime = Date.now();
+  let nonce = 0;
+  let hash = CryptoJS.SHA256(data + nonce).toString();
+
+  let isMineDone = hash.startsWith("0".repeat(complexity));
+
+  const timer = setInterval(() => {
+    if (isMineDone) {
+      const endTime = Date.now();
+      renderResult(nonce, hash, endTime - startTime);
+      clearInterval(timer);
+    }
+    nonce++;
+    hash = CryptoJS.SHA256(data + nonce).toString();
+    isMineDone = hash.startsWith("0".repeat(complexity));
+    renderResult(nonce, hash);
+  }, 0);
+}
+
+function renderResult(nonce, hash, msTime) {
+  let hashId = "hash";
+  let nonceId = "nonce";
+  let timeId = "time";
+
+  updateData(hashId, hash);
+  updateData(nonceId, nonce);
+  if (msTime) {
+    updateData(timeId, `${msTime / 1000} seconds`);
+  }
+}
+
+function updateData(id, data) {
+  const el = document.getElementById(id);
+  if (el) {
+    el.innerText = data;
+  }
+}
+
+function getData(id) {
+  let el = document.getElementById(id);
+  if (el) {
+    return el.value;
+  }
+}
